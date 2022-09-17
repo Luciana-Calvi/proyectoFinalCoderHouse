75ed679697d6040507c8e98bcff1f0234dec181d
Luci <lucalvi64@gmail.com>
2022-09-17 19:28:00 -0300

    Ajuste sobre reservas realizadas

--- reservasRealizadas.js
+++ reservasRealizadas.js
@@ -1,27 +1,26 @@
 let reserva = JSON.parse(localStorage.getItem("reservas"));
 
+llenarTabla();
+
 function llenarTabla(){
-   let contenido = '';
-
     let dpto1 = JSON.parse(localStorage.getItem("reservas"));
-
-    contenido = contenido + '<tr>';
-    contenido = contenido + '<th><div class="size text-center">DEPARTAMENTO</div></th>';
-    contenido = contenido + '<th><div class="size text-center">FECHA DESDE</div></th>';
-    contenido = contenido + '<th><div class="size text-center">FECHA HASTA</div></th>';
-    contenido = contenido + '</tr>';
-    dpto1.forEach(element => {
+    if (dpto1 != null) {
+        let contenido = '';
         contenido = contenido + '<tr>';
-        contenido = contenido + '<td>' + element[0].depto + '</td>';
-        contenido = contenido + '<td>' + element[0].fechaDesde + '</td>';
-        contenido = contenido + '<td>' + element[0].fechaHasta + '</td>';
+        contenido = contenido + '<th><div class="size text-center">DEPARTAMENTO</div></th>';
+        contenido = contenido + '<th><div class="size text-center">FECHA DESDE</div></th>';
+        contenido = contenido + '<th><div class="size text-center">FECHA HASTA</div></th>';
         contenido = contenido + '</tr>';
-    });
-    console.log(contenido);
-//meter then y cath aca
-    
-    document.querySelector('#tablaReservas').innerHTML = contenido;
+        dpto1.forEach(element => {
+            contenido = contenido + '<tr>';
+            contenido = contenido + '<td>' + element[0].depto + '</td>';
+            contenido = contenido + '<td>' + element[0].fechaDesde + '</td>';
+            contenido = contenido + '<td>' + element[0].fechaHasta + '</td>';
+            contenido = contenido + '</tr>';
+        });
+        //meter then y cath aca
+        document.querySelector('#tablaReservas').innerHTML = contenido;
+    }
     //let error = "error";
     //error ? tablaReservas.innerHTML = error : tablaReservas.innerHTML = contenido;
 }
-llenarTabla();
