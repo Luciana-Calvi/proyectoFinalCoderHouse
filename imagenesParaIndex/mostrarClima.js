b0f0c60f839b7dd8a8c41e355c710af7c2eb3cdd
Luci <lucalvi64@gmail.com>
2022-09-19 23:15:14 -0300

    renovar acreditacion de api

--- imagenesParaIndex/mostrarClima.js
+++ imagenesParaIndex/mostrarClima.js
@@ -1,7 +1,7 @@
 function obtenerClima() {
     let contenido = '';
 
-    fetch('http://api.weatherapi.com/v1/forecast.json?key=8378b63c1c234aa29a212755222908&q=Bahia Blanca&days=5&aqi=no&alerts=no')
+    fetch('http://api.weatherapi.com/v1/forecast.json?key=0c35cacad6d940ce88a21153222009&q=Bahia Blanca&days=5&aqi=no&alerts=no')
     .then(response => response.json()
     ).then(
         json => {
